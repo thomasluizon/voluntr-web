cb76cf6c26f9497accd359538a29993aaefbe5ea
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-10-14 21:00:14 -0300

    chore: add test flat setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
 })
