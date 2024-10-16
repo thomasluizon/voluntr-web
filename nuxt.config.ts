9bc59091ae0b1b63c64107a8b7b62abcaeb6b938
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-10-16 07:58:56 -0300

    chore: add nuxt tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils/module', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
 })
