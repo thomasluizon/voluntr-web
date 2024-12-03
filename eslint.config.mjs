20cf2514bf50ab7bcc2db9b0f4e5dc9ea9c0aa1c
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-03 20:47:03 -0300

    hotfix: remove vue/multi-word-component-names rule for shacn components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,23 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import stylistic from '@stylistic/eslint-plugin'
 
-export default withNuxt({
-  plugins: {
-    '@stylistic': stylistic
+export default withNuxt([
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/comma-dangle': ['error', 'never']
+    }
   },
-  rules: {
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/semi': ['error', 'never'],
-    '@stylistic/quotes': ['error', 'single'],
-    '@stylistic/comma-dangle': ['error', 'never'],
-    'no-console': 'warn'
+  {
+    // Condition for the `ui/` folder
+    files: ['src/components/ui/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
   }
-})
+])
