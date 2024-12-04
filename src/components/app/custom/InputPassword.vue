620d5a80ff6c7025c7a91b287543adab9dd3d87a
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-03 21:28:38 -0300

    feat: ensure input has a label

--- src/components/app/custom/InputPassword.vue
+++ src/components/app/custom/InputPassword.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+
 <template>
-  <Input type="password" />
+  <div class="flex flex-col space-y-2">
+    <Label>{{ label }}</Label>
+    <Input type="password" />
+  </div>
 </template>
