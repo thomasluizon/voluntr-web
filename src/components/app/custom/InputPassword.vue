e08cf302d3545d3b6aef125fe69f58d9f539f59a
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-03 22:33:33 -0300

    feat: ensure password visibility toggles on button click

--- src/components/app/custom/InputPassword.vue
+++ src/components/app/custom/InputPassword.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { EyeClosedIcon } from '@radix-icons/vue'
+import { EyeClosedIcon, EyeOpenIcon } from '@radix-icons/vue'
 
 defineProps<{
   id: string
@@ -7,6 +7,10 @@ defineProps<{
 }>()
 
 const isVisible = ref(false)
+
+const toggleVisibility = () => {
+  isVisible.value = !isVisible.value
+}
 
 defineExpose({
   isVisible
@@ -17,12 +21,13 @@ defineExpose({
   <div class="flex flex-col space-y-2">
     <Label :for="id">{{ label }}</Label>
    <div class="relative">
-      <Input :id="id" type="password" />
+      <Input :id="id" :type="isVisible ? 'text' : 'password'" />
       <button
         class="absolute inset-y-0 end-0 flex h-full w-10 items-center justify-center text-muted-foreground rounded-md outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
         aria-controls="password"
+        @click="toggleVisibility"
       >
-        <EyeClosedIcon class="w-5 h-5" aria-hidden="true" />
+       <component :is="isVisible ? EyeOpenIcon : EyeClosedIcon" class="w-5 h-5" />
     </button>
    </div>
   </div>
