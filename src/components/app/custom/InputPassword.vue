0f6c6f236ac384068c3fa4b5359081baa4cc0958
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-03 22:37:08 -0300

    style: fix indent

--- src/components/app/custom/InputPassword.vue
+++ src/components/app/custom/InputPassword.vue
@@ -20,7 +20,7 @@ defineExpose({
 <template>
   <div class="flex flex-col space-y-2">
     <Label :for="id">{{ label }}</Label>
-   <div class="relative">
+    <div class="relative">
       <Input :id="id" :type="isVisible ? 'text' : 'password'" />
       <button
         class="absolute inset-y-0 end-0 flex h-full w-10 items-center justify-center text-muted-foreground rounded-md outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
@@ -28,7 +28,7 @@ defineExpose({
         @click="toggleVisibility"
       >
        <component :is="isVisible ? EyeOpenIcon : EyeClosedIcon" class="w-5 h-5" />
-    </button>
-   </div>
+      </button>
+    </div>
   </div>
 </template>
