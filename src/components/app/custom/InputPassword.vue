a072d91e8bad358e0f146cd33ec3315696f40cb0
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-03 23:02:15 -0300

    feat: ensure extra attribute it will be passed

--- src/components/app/custom/InputPassword.vue
+++ src/components/app/custom/InputPassword.vue
@@ -21,7 +21,7 @@ defineExpose({
   <div class="flex flex-col space-y-2">
     <Label :for="id">{{ label }}</Label>
     <div class="relative">
-      <Input :id="id" :type="isVisible ? 'text' : 'password'" />
+      <Input :id="id" :type="isVisible ? 'text' : 'password'" v-bind="$attrs" />
       <button
         class="absolute inset-y-0 end-0 flex h-full w-10 items-center justify-center text-muted-foreground rounded-md outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
         aria-controls="password"
