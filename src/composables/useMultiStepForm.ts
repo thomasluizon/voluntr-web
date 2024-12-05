43393f6dd63ea262ba62f298b76dda02ba05c35e
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-12-05 17:10:03 -0300

    feat: ensure composable it will initilize correctly with default values

---
+++ src/composables/useMultiStepForm.ts
@@ -0,0 +1,17 @@
+type ValidationSchema = Array<Record<string, unknown>>
+
+type EmitFunction = (event: string, payload: unknown) => void
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function useMultiStepForm(validationSchema: ValidationSchema, emit: EmitFunction) {
+  const currentStepIdx = ref(0)
+
+  const isLastStep = computed(() => currentStepIdx.value === validationSchema.length - 1)
+  const hasPrevious = computed(() => currentStepIdx.value > 0)
+
+  return {
+    currentStepIdx,
+    isLastStep,
+    hasPrevious
+  }
+}
